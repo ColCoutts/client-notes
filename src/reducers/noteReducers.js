1e354a80b65907167fcff89943e81b1f042a7d7b
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-10 10:30:09 -0700

    updated reducer with new note/pending actions

--- src/reducers/noteReducers.js
+++ src/reducers/noteReducers.js
@@ -1,7 +1,16 @@
-const initialState = [];
+import { NEW_NOTE, NEW_NOTE_PENDING } from '../actions/notesActions';
+
+const initialState = {
+  loading: true,
+  list: []
+};
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case NEW_NOTE_PENDING:
+      return { ...state, loading: true };
+    case NEW_NOTE:
+      return { ...state, loading: false, list: [...state.list, action.payload] }; 
     default:
       return state;
   }
