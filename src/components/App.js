d10da7e0dec7e40796f23d7d5f05c74b3a030f54
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-11 14:04:29 -0700

    updated package and added home for page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import CreateNote from '../containers/CreateNote';
-import AllNotes from '../containers/AllNotes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
 
 
 export default function App() {
   return (
-    <>
-      <CreateNote />
-      <AllNotes />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
