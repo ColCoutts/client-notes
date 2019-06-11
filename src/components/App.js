b3364c1eff35f5f47ad185b151db174a0528525f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-11 14:26:34 -0700

    built out some auth containers

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,16 @@ import {
   Route
 } from 'react-router-dom';
 import Home from './Home';
+import Callback from '../containers/auth/Callback';
+import { withSession } from '../containers/auth/withSession';
 
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={withSession(Home)} />
+        <Route path="/callback" component={Callback} />
       </Switch>
     </Router>
   );
