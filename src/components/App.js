75b1f745e2f747b8c070a674b3f6595fab1b7fd4
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-11 10:19:29 -0700

    try to push with all notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CreateNote from '../containers/CreateNote';
+import AllNotes from '../containers/AllNotes';
+
 
 export default function App() {
   return (
     <>
       <CreateNote />
+      <AllNotes />
     </>
   );
 }
